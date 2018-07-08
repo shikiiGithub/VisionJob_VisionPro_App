5e84daceb2487339632f15ebcba2a8e6ace6ba48
shikii <123>
2018-07-08 22:08:18 +0800

    修正了日志查询的Bug

--- 主程序/RetriveLogForm.cs
+++ 主程序/RetriveLogForm.cs
@@ -19,11 +19,11 @@ namespace shikii.VisionJob
         private void btn_Search_Click(object sender, EventArgs e)
         {
             bool isSearchDay = false;
-            if(dateTimePicker1.Text.Equals(dateTimePicker2.Text))
+            if (dateTimePicker1.Text.Equals(dateTimePicker2.Text))
             {
                 isSearchDay = true;
             }
-            if(dateTimePicker2.Value <dateTimePicker2.Value)
+            if (dateTimePicker2.Value < dateTimePicker2.Value)
             {
                 dotNetLab.Tipper.Error = "第二个日期控件的日期必须大于或者等于前一个日期控件的日期。";
                 return;
@@ -32,13 +32,14 @@ namespace shikii.VisionJob
             {
                 String searchTime = dateTimePicker1.Value.ToString("yyyy-MM-dd");
 
-               this.dataGridView1.DataSource =  dotNetLab.Common.R.LogDB.ProvideTable(String.Format("SELECT Fire_Time,Message FROM LogTable where Fire_Time='{0}';", searchTime), dotNetLab.Data.DBOperator.OPERATOR_QUERY_TABLE);
+                this.dataGridView1.DataSource = dotNetLab.Common.R.LogDB.ProvideTable(String.Format("SELECT Fire_Time,Message FROM LogTable where Fire_Time like '{0}%'; ", searchTime), dotNetLab.Data.DBOperator.OPERATOR_QUERY_TABLE);
              }
             else
             {
                 String searchTime1 = dateTimePicker1.Value.ToString("yyyy-MM-dd");
-                String searchTime2 = dateTimePicker2.Value.ToString("yyyy-MM-dd");
-                this.dataGridView1.DataSource = dotNetLab.Common.R.LogDB.ProvideTable(String.Format("SELECT Fire_Time,Message FROM LogTable where Fire_Time >='{0}' and Fire_Time <='{1}';", searchTime1,searchTime2), dotNetLab.Data.DBOperator.OPERATOR_QUERY_TABLE);
+                DateTime dt = dateTimePicker1.Value.AddDays(1);
+                String searchTime2 = dt.ToString("yyyy-MM-dd");
+                this.dataGridView1.DataSource = dotNetLab.Common.R.LogDB.ProvideTable(String.Format("SELECT Fire_Time,Message FROM LogTable where Fire_Time >= '{0}' and Fire_Time < '{1}'; ", searchTime1,searchTime2), dotNetLab.Data.DBOperator.OPERATOR_QUERY_TABLE);
             }
         }
     }
